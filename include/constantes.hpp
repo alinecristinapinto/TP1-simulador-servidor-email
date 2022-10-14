026362df26c78c6e68e1f8ad2696c546f63e802d
Aline Cristina Pinto <alinecristina3.14nto@gmail.com>
2022-10-13 23:24:05 -0300

    feat: adds operation constants

---
+++ include/constantes.hpp
@@ -0,0 +1,9 @@
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+
+#include <string>
+
+const std::string OPERACAO_CADASTRAR = "CADASTRA";
+const std::string OPERACAO_CONSULTA = "CONSULTA";
+const std::string OPERACAO_ENTREGA = "ENTREGA";
+const std::string OPERACAO_FIM = "FIM";
